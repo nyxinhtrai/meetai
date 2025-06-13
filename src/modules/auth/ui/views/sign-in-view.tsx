bd067b20a628b76efdbd4bcf7d60bbde2bf26ddb
Ny Nguyen <npsony2@gmail.com>
2025-06-13 14:01:29 +0700

    update logo

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -162,7 +162,7 @@ export const SignInView = () => {
                         </form>
                     </Form>     
                     <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
-                        <img src="/ailogo.png" alt="Image" className="h-[92px] w-[92px]" />
+                        <img src="/ailogo.png" alt="Image" className="h-[162px] w-[162px]" />
                         <p className="text-2xl font-semibold text-white">
                             Meet.AI
                         </p>
--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -209,7 +209,7 @@ export const SignUpView = () => {
                         </form>
                     </Form>     
                     <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
-                        <img src="/ailogo.png" alt="Image" className="h-[92px] w-[92px]" />
+                        <img src="/ailogo.png" alt="Image" className="h-[162px] w-[162px]" />
                         <p className="text-2xl font-semibold text-white">
                             Meet.AI
                         </p>
