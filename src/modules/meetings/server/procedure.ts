1013deb6220222985b91ef881a6a4d44f9e6cc50
Ny Nguyen <npsony2@gmail.com>
2025-06-18 11:05:36 +0700

    meetings form

--- src/modules/meetings/server/procedure.ts
+++ src/modules/meetings/server/procedure.ts
@@ -5,8 +5,47 @@ import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
+import { meetingsInsertSchema, meetingsUpdateSchema } from "../schema";
 
 export const meetingsRouter = createTRPCRouter({
+    update: protectedProcedure
+        .input(meetingsUpdateSchema)
+        .mutation(async ({ ctx, input }) => {
+            const [updatedMeeting] = await db
+                .update(meetings)
+                .set(input)
+                .where(
+                    and(
+                        eq(meetings.id, input.id),
+                        eq(meetings.userId, ctx.auth.user.id),
+                    )
+                )
+                .returning();
+                
+            if (!updatedMeeting) {
+                throw new TRPCError({
+                    code: 'NOT_FOUND',
+                    message: 'Meeting not found'
+                });
+            }
+
+            return updatedMeeting;
+        }),
+    create: protectedProcedure
+            .input(meetingsInsertSchema)
+            .mutation(async ({ input, ctx }) => {
+                const [createdMeeting] = await db
+                    .insert(meetings)
+                    .values({
+                        ...input,
+                        userId: ctx.auth.user.id,
+                    })
+                    .returning();
+
+                //TODO: Create Stream Call, Upsert Stream Users
+    
+                return createdMeeting;
+            }),
     getOne: protectedProcedure
         .input(z.object({ id: z.string() }))
         .query(async ({ input, ctx }) => {
@@ -23,7 +62,7 @@ export const meetingsRouter = createTRPCRouter({
             );
 
             if (!existingMeeting) {
-                throw new TRPCError({ code: 'NOT_FOUND', message: 'Agent not found' });
+                throw new TRPCError({ code: 'NOT_FOUND', message: 'Meeting not found' });
             }
 
         return existingMeeting;
